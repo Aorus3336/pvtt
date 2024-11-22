1f49fd9452d507ddc4523f745a96f493c2428355
Aorus3336 <aorus3336@gmail.com>
2024-11-22 11:35:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+
+BOT_TOKEN=("7856318383:AAFv1FL5rSW5xjTK6rpo5w_qBzeU0l8Xuus")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@VirtualVenture")
+
+ADMIN_IDS=("5134043595")
+
+
+
+
+
+
+
+  
